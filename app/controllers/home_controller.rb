0dc0fb490b685847c2302f425dd8bfc959fbd0a0
nicolas kennedy <nico@mvpbuilders.tech>
2025-06-16 18:46:33 -0300

    mejoras cards + explorar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @best_clothing  = Product.where(tipo: "ropa", available: true).limit(8)
-    @best_furniture = Product.where(tipo: "muebles", available: true).limit(8)
-    @best_kids      = KidsProduct.where(available: true).limit(8)
-    @best_uniforms  = UniformProduct.where(available: true).limit(8)
-    @best_others    = OtherProduct.where(available: true).limit(8)
+    @best_clothing  = Product.where(tipo: "ropa", available: true).limit(20)
+    @best_furniture = Product.where(tipo: "muebles", available: true).limit(20)
+    @best_kids      = KidsProduct.where(available: true).limit(20)
+    @best_uniforms  = UniformProduct.where(available: true).limit(20)
+    @best_others    = OtherProduct.where(available: true).limit(20)
+    @best_accessory   = AccessoryProduct.where(available: true).limit(20)
   end
 end
