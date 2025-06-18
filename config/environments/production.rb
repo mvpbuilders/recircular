ea4569e2c73c364c63f9f8c00149d211e4025338
nicolas kennedy <nico@mvpbuilders.tech>
2025-06-18 12:26:29 -0300

    cambios config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  Rails.application.default_url_options[:host] = "https://www.shopcircu.com"
+  routes.default_url_options[:host] = "https://www.shopcircu.com"
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_caching = false
+  config.action_mailer.asset_host = "https://www.shopcircu.com"
+  config.action_mailer.default_url_options = { host: "https://www.shopcircu.com" }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
